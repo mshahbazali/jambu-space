fa0f38b14c7850488aa3cd4d2c35ff3379b05eee
Shahbaz Ali <shahbazattari121@gmail.com>
2022-08-08 21:37:17 -0700

    Updated

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ const io = require("socket.io")(server, {
 const users = []
 
 io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
 	socket.on('create-conversion', async (user) => {
 		const conversations = await Conversation.find({
 			members: { $in: [user.receiverID] },
